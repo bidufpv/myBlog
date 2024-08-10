e7061e1f3c7c0fcd21ae66940f3c8780e534b78c
bidufpv <bidufpv@gmail.com>
2024-08-10 20:05:35 +0530

    commit c

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { AuthLayout, Login } from './components/index.js'
 import AddPost from "./pages/AddPost.jsx";
 import Signup from './pages/Signup.jsx'
 import EditPost from "./pages/EditPost.jsx";
-import Home from './pages/Home.jsx'
+import {Home} from './pages/Home.jsx'
 
 import Post from "./pages/Post.jsx";
 
