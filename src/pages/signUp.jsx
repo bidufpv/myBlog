8098ced24a833ef7c4ab8115061e32e3c1c0d35d
bidufpv <bidufpv@gmail.com>
2024-08-11 21:39:45 +0530

    commit e

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Signup as SignupComponent } from '../components'
 
-function Signup() {
+const Signup = ()=> {
   return (
     <div className='py-8'>
         <SignupComponent />
