b72d8168eece112696f60350680b3cd207693c31
bidufpv <bidufpv@gmail.com>
2024-08-21 19:36:03 +0530

    defineconig added

---
+++ define.config.js
@@ -0,0 +1,26 @@
+// export default defineConfig({
+//     build: {
+//       rollupOptions: {
+//         // https://rollupjs.org/configuration-options/
+//       },
+//     },
+//   })
+
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default (async () => ({
+	input: 'main.js',
+	plugins: [
+		resolve(),
+		commonjs(),
+		isProduction && (await import('@rollup/plugin-terser')).default()
+	],
+	output: {
+		file: 'bundle.js',
+		format: 'cjs'
+	}
+}))();
